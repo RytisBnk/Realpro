676ac1dac7e1c067a72911e718aed3f517a465ab
Povilas <povilas935@gmail.com>
2018-06-22 20:23:31 +0300

    Frontendas cancer

---
+++ nt/checkout/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,500" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <title>Apmokejimas</title>
+    <link rel="stylesheet" href="../css/scss.css">
+  </head>
+  <body>
+    <a href='#'><img src='../img/logo.png' class='logo'></a>
+    <div class='afterLogo'>
+    <h2>Užsakymas</h2>
+
+    <div class="field is-grouped is-grouped-centered">
+<p class="control has-icons-left">
+  <span class="select">
+    <select>
+      <option selected>Namas</option>
+      <option>Butas</option>
+      <option>Sklypas</option>
+    </select>
+  </span>
+  <span class="icon is-small is-left">
+    <i class="fas fa-home"></i>
+  </span>
+</p>
+<p class="control">
+  <span class="select">
+    <select>
+      <option selected>Parduoti</option>
+      <option>Nuomuoti</option>
+    </select>
+  </span>
+</p>
+</div>
+<div class="field">
+  <label class="label">Vardas</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Vardas">
+  </div>
+</div>
+  <div class='field'>
+  <label class="label">Pilnas objekto adresas</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Pvz. Vilniaus g. 20-55, Vilnius, Lietuva">
+  </div>
+</div>
+<div class='field'>
+<label class="label">Plotas m<sup>2</sup></label>
+<div class="control">
+  <input class="input" type="number" placeholder="Plotas">
+</div>
+</div>
+<div class='field'>
+<label class="label">Kambarių sk.</label>
+<div class="control">
+  <input class="input" type="number" placeholder="Kambarių sk.">
+</div>
+</div>
+<div class='field'>
+<label class="label">Aukštas</label>
+<div class="control">
+  <input class="input" type="number" placeholder="Aukštas">
+</div>
+</div>
+<div class='field'>
+<label class="label">Aukštų sk.</label>
+<div class="control">
+  <input class="input" type="number" placeholder="Aukštų sk.">
+</div>
+</div>
+<div class='field'>
+<label class="label">Statybos metai</label>
+<div class="control">
+  <input class="input" type="number" placeholder="Aukštų sk.">
+  <label class="checkbox">
+    <input type="checkbox">
+    Namas renovuotas
+  </label>
+</div>
+</div>
+</div>
+  </body>
+</html>
