4f8ee48f2c65f987700c8151f4e405f63f257207
Povilas <povilas935@gmail.com>
2018-06-28 19:38:54 +0300

    added name attributes and form

--- app/resources/views/checkout.blade.php
+++ app/resources/views/checkout.blade.php
@@ -12,14 +12,14 @@
     <a href='#'><img src='../img/logo.png' class='logo'></a>
     <div class='afterLogo'>
     <h2>Užsakymas</h2>
-
+    <form method="POST" action='/order'>
     <div class="field is-grouped is-grouped-centered">
 <p class="control has-icons-left">
   <span class="select">
-    <select>
-      <option selected>Namas</option>
-      <option>Butas</option>
-      <option>Sklypas</option>
+    <select name='tipas'>
+      <option selected value='namas'>Namas</option>
+      <option value="butas">Butas</option>
+      <option value="sklypas">Sklypas</option>
     </select>
   </span>
   <span class="icon is-small is-left">
@@ -28,9 +28,9 @@
 </p>
 <p class="control">
   <span class="select">
-    <select>
-      <option selected>Parduoti</option>
-      <option>Nuomuoti</option>
+    <select name='arParduoti'>
+      <option selected value='parduoti'>Parduoti</option>
+      <option value='nuomuoti'>Nuomuoti</option>
     </select>
   </span>
 </p>
@@ -38,49 +38,50 @@
 <div class="field">
   <label class="label">Vardas</label>
   <div class="control">
-    <input class="input" type="text" placeholder="Vardas">
+    <input class="input" type="text" placeholder="Vardas" name='vardas'>
   </div>
 </div>
   <div class='field'>
   <label class="label">Pilnas objekto adresas</label>
   <div class="control">
-    <input class="input" type="text" placeholder="Pvz. Vilniaus g. 20-55, Vilnius, Lietuva">
+    <input class="input" type="text" placeholder="Pvz. Vilniaus g. 20-55, Vilnius, Lietuva" name='adresas'>
   </div>
 </div>
 <div class='field'>
 <label class="label">Plotas m<sup>2</sup></label>
 <div class="control">
-  <input class="input" type="number" placeholder="Plotas">
+  <input class="input" type="number" placeholder="Plotas" name='plotas'>
 </div>
 </div>
 <div class='field'>
 <label class="label">Kambarių sk.</label>
 <div class="control">
-  <input class="input" type="number" placeholder="Kambarių sk.">
+  <input class="input" type="number" placeholder="Kambarių sk." name='kambariu_skaicius'>
 </div>
 </div>
 <div class='field'>
 <label class="label">Aukštas</label>
 <div class="control">
-  <input class="input" type="number" placeholder="Aukštas">
+  <input class="input" type="number" placeholder="Aukštas" name='aukstas'>
 </div>
 </div>
 <div class='field'>
 <label class="label">Aukštų sk.</label>
 <div class="control">
-  <input class="input" type="number" placeholder="Aukštų sk.">
+  <input class="input" type="number" placeholder="Aukštų sk." name='aukstu_skaicius'>
 </div>
 </div>
 <div class='field'>
 <label class="label">Statybos metai</label>
 <div class="control">
-  <input class="input" type="number" placeholder="Aukštų sk.">
+  <input class="input" type="number" placeholder="Statybos metai" name="statybos_metai">
   <label class="checkbox">
-    <input type="checkbox">
+    <input type="checkbox" name='renovuotas'>
     Namas renovuotas
   </label>
 </div>
 </div>
 </div>
+</form>
   </body>
 </html>
