485bd214df91f7977d1db0ac704d77997bec67a9
Povilas <povilas935@gmail.com>
2018-07-15 16:20:20 +0300

    initial set up

---
+++ resources/views/order/list.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,500" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <title>Jūsų Užsakymai</title>
+    <link rel="stylesheet" href="../../css/scss.css">
+  </head>
+  <body>
+    <a href='#'><img src='../img/logo.png' alt='Realpro' class='logo'></a>
+    awfawfafafas
+  </body>
+</html>
