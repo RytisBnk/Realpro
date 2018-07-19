d83a0b133baf7b398d40e1562d8a4a2aae9c3dbf
RytisBnk <rytis.bankieta@gmail.com>
2018-07-19 23:03:00 +0300

    Fix undefined route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/plans', function(){
     return view('plans');
-});
+})->name('plans');;
 
 Route::get('/terms', function(){
     return view('terms');
