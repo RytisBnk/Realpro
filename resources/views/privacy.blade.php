d947545c8d143ddc7e99df54053b64fb2cb33b4b
HenrikasB <35893609+HenrikasB@users.noreply.github.com>
2018-07-20 12:30:42 +0300

    Henrix (#12)
    * Privacy
    
    Pridėta papildoma privatumo informacija
    
    * Privacy
    
    atnaujintas ir aprasytas privacy

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,500" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/scss.css">
+<title>Sąlygos | RealPro</title>
+</head>
+
+<body class='has-navbar-fixed-top terms'>
+
+  <nav class="navbar is-fixed-top my-navbar">
+  <div class="navbar-brand">
+  <div class="navbar-burger burger" data-target="my-navbar-menu">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+  </div>
+  <div id="my-navbar-menu" class="navbar-menu">
+  <div class="navbar-start">
+    <div class="navbar-item" style="margin-right: -80px;"><a href="http://realpro.test/#paslaugos" style='z-index: 999999;' class='nav-item' data-ripple="rgba(0,0,0, 0.3)">Paslaugos</a>
+    </div>
+    <div class="navbar-item" style="margin-right: -120px;"><a class='nav-item' style='z-index: 999999;' data-ripple="rgba(0,0,0, 0.3)" href="#kainos">Kainos</a>
+    </div>
+  </div>
+  <div class="my-navbar-center">
+    <div class="navbar-item"><a href='#'><img src="img/logo.png" alt="Realpro NT agentura" style="padding-top: 20px; width: 150px;"></a>
+    </div>
+  </div>
+  <div class="navbar-end">
+    <div class="navbar-item" style="margin-left: -80px;"><a class='nav-item'  data-ripple="rgba(0,0,0, 0.3)" href="#duk">DUK</a>
+    </div>
+    <div class="navbar-item" style="margin-left: -20px;"><a class='nav-item'  data-ripple="rgba(0,0,0, 0.3)" href="#apie">Apie Mus</a>
+    </div>
+    @guest
+    <div class="navbar-item" style="margin-right: -60px;"><a data-ripple="rgba(0,0,0, 0.3)" class="button" href="{{ route('login') }}" id="login">Prisijungti</a>
+    @else
+    <div class="navbar-item" style="margin-right: -60px;"><a data-ripple="rgba(0,0,0, 0.3)" class="button" href="{{ route('order.list') }}" id="orders">Užsakymai</a>
+    @endguest
+    </div>
+  </div>
+  </div>
+  </nav>
+<div class='container'>
+	<h4>Bendra informacija</h4>
+		<p>"RealPro" gerbia savo Lankytojų ir Klientų privatumą, todėl garantuojame, kad realpro.lt  internetinėje sistemoje Jūsų pateikta informacijanaudojama ir laikoma saugiai. Tačiau prieš darydami užsakymą perskaitykite šias taisykles, kad vėliau nekiltų nesusipratimų.
+	"RealPro" pasilieka teisę bet kuriuo metu šias Taisykles keisti, todėl kaskart naudodamiesi mūsų sistema turite iš naujo jas perskaityti. Naudodamiesi "RealPro" elektroninės sistemos teikiamomis paslaugomis Jūs patvirtinate, kad sutinkate laikytis šių Taisyklių.</p>
+
+	<h4>Renkama informacija</h4>
+		<p>Visi mūsų Klientai yra vienodai svarbūs! Tačiau tokia informacija kaip Jūsų vardas, pavardė, adresas, elektroninis pašto adresas, telefono numeris, reikalinga tam, kad galėtume patvirtinti Jūsų užsakymą ir lsusisiekti su Jumis dėl mūsų teikiamų paslaugų.
+	Mes nesudarinėjame jokių klientų sąrašų ir neperduodame jų tretiesiems asmenims, kurie nesusiję su Jūsų užsakymu. Surinkta Jūsų asmeninė informacija lieka pas mus. Šią informaciją naudojame siekdami teikti paslaugas, jas palaikyti, susisiekti su Jumis dėl užsakymų ar kitų iškilusių klausimų, pristatant naujienas ar išrašant PVM sąskaitą-faktūrą ir pan.
+	Mes nesidalijame Jūsų asmenine informacija su įmonėmis ar asmenimis, išskyrus tam tikrus, čia pateiktus atvejus. Norėdami įvykdyti tam tikrus būtinus veiksmus – įvykdyti užsakymus, užskaityti apmokėjimus.. Visa asmeninė Pirkėjo informacija tvarkoma remiantis galiojančiais įstatymais ir teisės aktais. Be Jūsų sutikimo Jūsų asmeniniai duomenys nebus naudojami kitais nei išvardintais tikslais, nebent to reikalaus galiojantys įstatymai ar teisės aktai.
+	Paslaugos pirkėjas pateikdamas mums savo asmeninius duomenis sutinka, kad mes galime rinkti, naudoti ir atskleisti asmens duomenis aukščiau išvardytais tikslais</p>
+
+	<h4>Duomenų apsauga</h4>
+		<p>"RealPro" internetinėje svetainėje stengiasmasi, kad Jūsų pateikti asmeniniai duomenys būtų saugūs. „RealPro“ visada stengiasi apsaugoti informaciją nuo neteisėtos prieigos, neteisėto jos pakeitimo, atskleidimo ar sunaikinimo.
+	Atkreipiame dėmesį, kad mes niekada neprašome elektroniniuose laiškuose nurodyti slaptažodžio. Pirkėjas atsakingas už savo sąskaitos informacijos ir slaptažodžio slaptumą. Visi kiti Jūsų asmeniniai duomenys, yra saugomi, mes imamės atitinkamų fizinių, elektroninių ir valdymo priemonių apsaugoti renkamai informacijai.</p>
+
+	<h4>Privatumo politikos pakeitimai</h4>
+		<p>"RealPro“ turi teisę keisti šią privatumo politiką. Tolimesnis naudojimasis mūsų tinklapio paslaugomis reiškia šių taisyklių laikymąsi. Visi iškilę nesutarimai ir ginčai sprendžiami derybu būdu. Nepavykus susitarti, ginčai sprendžiami pagal LR įstatymus.</p>
+</div>
+</body>
+</html>
