9e538e7be3dab5a212267aabe1d500c1a3cf4427
RytisBnk <rytis.bankieta@gmail.com>
2018-07-23 22:01:57 +0300

    Revert "Fix to assets not loading"
    This reverts commit 15cfe7eff4fc40006d8c4605c810af170d422bb9.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,12 +11,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-        if (env('APP_ENV') !== local)
-        {
-            $url->forceSchema('https');
-        }
+        //
     }
 
     /**
