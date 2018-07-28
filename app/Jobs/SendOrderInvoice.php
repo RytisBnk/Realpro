be668b2e32510292a2fd47e9bd056253dee66867
RytisBnk <rytis.bankieta@gmail.com>
2018-07-28 21:25:28 +0300

    Error fix

--- app/Jobs/SendOrderInvoice.php
+++ app/Jobs/SendOrderInvoice.php
@@ -50,7 +50,6 @@ class SendOrderInvoice implements ShouldQueue
         $invoice->setFrom(array("Seller Name","Sample Company Name","128 AA Juanita Ave","Glendora , CA 91740"));
         $invoice->setTo(array("Purchaser Name","Sample Company Name","128 AA Juanita Ave","Glendora , CA 91740"));
 
-        
-        Mail::to($user->email)->send(new OrderCompleted($this->filepath));
+        Mail::to($this->user->email)->send(new OrderCompleted($this->filepath));
     }
 }
