a402d4accaf0e2e1e91a7d9ead745546e2796080
RytisBnk <rytis.bankieta@gmail.com>
2018-10-06 00:37:14 +0300

    Missing favicons and meta tags

--- resources/views/layouts/parallax.blade.php
+++ resources/views/layouts/parallax.blade.php
@@ -3,8 +3,16 @@
 
 <head>
     <meta charset="utf-8">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123099861-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-123099861-1');
+    </script>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no">
     <!-- description -->
     <meta name="description" content="Internetiniai nekilnojamo turto brokeriai. Minimalaus plano kaina TIK 99eur! Parduosime Jūsų nekilnojamą turtą geriausia kaina! Jokių paslėptų mokęsčių!">
     <!-- keywords -->
@@ -12,9 +20,17 @@
     namai,parduoti,butai,namai,brokeriai,komersinis,prekyba,sklypas,pigiausia,99eur">
     <!-- title -->
     <title>@yield('title')</title>
+    <meta name="verify-paysera" content="138aff64c31a47ce864635baaaa228e1">
     <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="https://realpro.lt/img/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="https://realpro.lt/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="https://realpro.lt/img/favicon-16x16.png">
+    <link rel="manifest" href="https://realpro.lt/img/site.webmanifest">
+    <link rel="mask-icon" href="https://realpro.lt/img/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="https://realpro.lt/img/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="https://realpro.lt/img/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
     <!-- animation -->
     <link rel="stylesheet" href="css/animate.min.css" />
     <!-- bootstrap -->
